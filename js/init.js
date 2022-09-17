27577a4e99001e019484b334ca48712a1aaff062
Karen <est.Kpaza@uml.edu.ni>
2022-09-17 13:31:32 -0600

    Creacion de la pagina web.

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(function () {
+
+        $('.sidenav').sidenav();
+        $('.materialboxed').materialbox();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
